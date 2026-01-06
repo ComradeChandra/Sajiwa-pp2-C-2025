002c33e0fe1048a676b217b5af50aba95b394fa3
ComradeChandra <chandrahr@gmail.com>
2026-01-06 17:06:02 +0700

    Fix: Inisialisasi Git di root folder agar pom.xml terbaca

---
+++ src/main/java/id/ac/unpas/sajiwa/App.java
@@ -0,0 +1,13 @@
+package id.ac.unpas.sajiwa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
